2a1c8449b74d2086994f26342414d96a690b7ac3
lily-tsou <tsou@pdx.edu>
2022-02-23 14:48:52 -0800

    Add functionality for declaring a winner

--- cryptogame.js
+++ cryptogame.js
@@ -7,6 +7,8 @@ const player2 = new Tozny.storage.Client(player2config)
 const judgeconfig = require('./client3config')
 const judge = new Tozny.storage.Client(judgeconfig)
 
+let moves = ["scissors", "paper", "rock"];
+
 /**
  * Share a record of a given type.
  */
@@ -29,9 +31,10 @@ async function share(sharer, receiverId, type) {
  * Player 1, Player 2, and the judge.
  */
  async function initializeAccess(){
-    share(player1, judgeconfig.clientId, 'move')
-    share(player2, judgeconfig.clientId, 'move')
-    share(player2, player1config.clientId, 'move')
+     //TODO set up with round 0
+    share(player1, judgeconfig.clientId, 'moveb')
+    share(player2, judgeconfig.clientId, 'moveb')
+    share(player2, player1config.clientId, 'moveb')
 }
 
 /**
@@ -50,9 +53,9 @@ async function revoke(revoker, revokeFromId, type){
  * Revoke all access granted in initializeParties()
  */
 async function revokeAllAccess(){
-    revoke(player1, judgeconfig.clientId, 'move')
-    revoke(player2, judgeconfig.clientId, 'move')
-    revoke(player2, player1config.clientId, 'move')
+    revoke(player1, judgeconfig.clientId, 'moveb')
+    revoke(player2, judgeconfig.clientId, 'moveb')
+    revoke(player2, player1config.clientId, 'moveb')
 }
 
 /**
@@ -62,7 +65,7 @@ async function revokeAllAccess(){
 async function getRound(player){
     try {
         const request = new Tozny.types.Search(true)
-        request.match({ type: 'move'})
+        request.match({ type: 'moveb'})
         const resultQuery = await player.search(request)
         const found = await resultQuery.next()
         return parseInt(found[found.length - 1].data.round);
@@ -71,27 +74,28 @@ async function getRound(player){
       }
 }
 
-async function setupMove(playerName, move){
+async function submitMove(playerName, move){
     console.log(`Submitting move for ${playerName}`)
     if(playerName === 'player1'){
         let round = await getRound(player1)
         recordMove(player1, move, round + 1)
-
-
     }
+
     else if(playerName === 'player2'){
         let round = await getRound(player2)
         recordMove(player2, move, round + 1)
     }
+
     else{
         console.log("Not a valid player")
     }
 }
 
 async function recordMove(player, moveChoice, roundNumber){
+    //TODO make round metadata
     try {
         const submitted = await player.writeRecord(
-          'move',
+          'moveb',
           {
             move: moveChoice.toString(),
             round: roundNumber.toString(),
@@ -109,10 +113,41 @@ async function recordMove(player, moveChoice, roundNumber){
       }
 }
 
+async function declareWinner(round){
+    let move1 = await getMove(player1config.clientId, round)
+    let move2 = await getMove(player2config.clientId, round)
+    console.log(round + " " + move1 + " " + move2)
+
+    if(move1 === move2)
+        console.log("draw")
+    else{
+        if(moves[(moves.indexOf(move1) + 1) % 3] === move2){
+            console.log("player 1 is the winner")
+        }
+        else
+            console.log("player 2 is the winner")
+    }
+}
+
+async function getMove(playerId, round){
+    try {
+        const request = new Tozny.types.Search(true, true)
+        request.match({ type: 'moveb', writers: playerId}, 'AND', 'EXACT')
+        const resultQuery = await judge.search(request)
+        const moves = await resultQuery.next()
+        for (let move of moves) {
+          if(parseInt(move.data.round) === round){
+            return move.data.move;
+        }
+        }
+        console.error(`No move submitted for ${playerId} for round ${round}`)
+      } catch(e) {
+        console.error(e)
+      }
+}
+
 async function main(){
-    //initializeAccess()
-    //revokeAllAccess()
-    setupMove("player2", "scissors")
+
 }
 
 main()
